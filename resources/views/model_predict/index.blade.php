e1196d2a4ed5c95cfea76d088c19b326fb6b37a3
pr3a <wisit.lertsakwimarn@gmail.com>
2017-06-20 06:35:34 +0700

    Fix View nodel_predict/index

--- resources/views/model_predict/index.blade.php
+++ resources/views/model_predict/index.blade.php
@@ -1,14 +1,39 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container-fluid">
-    <div class="row">
-        <div class="col-md-10 col-md-offset-1">
-            <a href="{{ url('model_predict/create') }}" class="btn btn-lg btn-primary btn-block">Create Model</a> 
-            <hr/>
-            <div class="panel panel-custom-horrible-blue">
-                <div class="panel-heading">
-                    <h4>Prediction Model Weather จำนวน {{ $models->total() }} โมเดล</small></h4>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-10 col-md-offset-1">
+                <div class="row">
+                    <div class="col-md-10">
+                        <span class="fa fa-database fa-5x"></span>
+                        <span style="font-size:38px; margin-left: 2%">รายการโมเดลพยากรณ์ฝนตก</span>
+                    </div>
+                    <div class="col-md-2">
+                        <div class="col-md-5"></div>
+                        <div class="col-md-7">
+                            <br>
+                            <a href="{{ url('/model_predict/create') }}" class="btn btn-blue">
+                                <span class="fa fa-plus-circle"></span> Create Model
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <hr/>
+                <div class="row">
+                    <div class="col-md-10"></div>
+                    <div class="col-md-2">
+                        {!! Form::open(['method' => 'GET','url'=>'/model_predict','class'=>'','role','search' ])!!}
+                        <div class="input-group custom-search-form">
+                            <input type="text" name="search" class="form-control" placeholder="ค้นหา ชื่อโมเดล">
+                            <span class="input-group-btn">
+                                <button type="submit" class="btn btn-default-sm">
+                                <i class="fa fa-search"></i>
+                                </button>
+                            </span>
+                        </div>
+                        {!! Form::close() !!}
+                    </div>
                 </div>
                 <div class="panel-body">
                     <div class="table-responsive">
