021d11558f5e2d21ecdcb7f676c71b2a91db0ef8
pr3a <wisit.lertsakwimarn@gmail.com>
2017-06-21 04:03:34 +0700

    Add map full

--- app/Http/Controllers/MapController.php
+++ app/Http/Controllers/MapController.php
@@ -15,26 +15,41 @@ class MapController extends Controller
         $this->middleware('auth');
     }
 
-    public function index()
+    public function index(Request $request)
     {
         $devices = Device::all();
-        Mapper::map(13.778797, 100.560264, ['zoom' => '6', 'minZoom' => '0', 'maxZoom' => '15','draggable' => true]);
 
-        foreach ($devices as $device) {
-            //Mapper::marker($device->latitude, $device->longitude,['title' => $device->SerialNumber ]);
-            $response = Geocode::make()->latLng($device->latitude, $device->longitude);
-            $content = '<b>'.$device->SerialNumber.'</b><br/>';
-            if ($response) {
-                Mapper::informationWindow($device->latitude, $device->longitude, $content.$response->formattedAddress() , ['open' => true, 'maxWidth' => 300, 'eventClick' => 'alert("left click");']);
-            } else {
-                Mapper::informationWindow($device->latitude, $device->longitude, $content, ['open' => true, 'maxWidth' => 300, 'eventClick' => 'alert("left click");']);
+        $latitude = $request->get('latitude');
+        $longitude = $request->get('longitude');
+        if (empty($latitude) || empty($longitude)) {
+            Mapper::map(0, 0, ['zoom' => '6', 'center' => false, 'marker' => false]);
+            foreach ($devices as $device) {
+                //Mapper::marker($device->latitude, $device->longitude,['title' => $device->SerialNumber ]);
+                $response = Geocode::make()->latLng($device->latitude, $device->longitude);
+                $content = '<b>' . $device->SerialNumber . '</b><br/>';
+                if ($response) {
+                    Mapper::informationWindow($device->latitude, $device->longitude, $content . $response->formattedAddress(), ['open' => true, 'maxWidth' => 300, 'eventClick' => 'alert("left click");']);
+                } else {
+                    Mapper::informationWindow($device->latitude, $device->longitude, $content, ['open' => true]);
+                }
             }
-        }
-
-        $response = Geocode::make()->latLng(13.778797, 100.560264);
-        if ($response) {
-            // echo $response->formattedAddress();
+        } else {
+            Mapper::map($latitude, $longitude, ['zoom' => '17', 'minZoom' => '0', 'maxZoom' => '15']);
         }
         return view('map.index');
+    }
+
+    public function mapfull(Request $request)
+    {
+        $latitude = $request->get('latitude');
+        $longitude = $request->get('longitude');
+        if (empty($latitude) || empty($longitude)) {
+            return abort(404);
+        } else {
+            $content = '<b>' . $latitude . ',' . $longitude . '</b><br/>';
+            Mapper::map(0, 0, ['zoom' => '16', 'marker' => false, 'center' => false, 'maxZoom' => '16']);
+            Mapper::informationWindow($latitude, $longitude, $content, ['open' => true , 'icon' => url('/images/humidity.png')]);
+        }
+        return view('map.full');
     }
 }
