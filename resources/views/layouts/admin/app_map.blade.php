95b055bded25137d1c79b53a02bdd58153e49431
pr3a <wisit.lertsakwimarn@gmail.com>
2017-07-06 01:18:22 +0700

    Edit select navbar to active

--- resources/views/layouts/admin/app_map.blade.php
+++ resources/views/layouts/admin/app_map.blade.php
@@ -48,10 +48,7 @@
             <div class="collapse navbar-collapse" id="app-navbar-collapse">
                 <!-- Left Side Of Navbar -->
                 <ul class="nav navbar-nav">
-                    <li><a href="{{ url('/admin/map') }}"><i class="fa fa-map-marker"></i> แผนที่</a></li>
-                    <li><a href="{{ url('/admin/devices/') }}"><i class="fa fa-mobile"></i> รายการอุปกรณ์IoT</a></li>
-                    <li><a href="{{ url('/admin/model_predicts') }}"><i class="fa fa-database"></i> โมเดลพยากรณ์</a></li>
-                    <li><a href="{{ url('/admin/report') }}"><i class="fa fa-cloud"></i> รายงานสภาพอากาศ</a></li>
+                  @include('includes.navbarLeft')
                 </ul>
                 <!-- Right Side Of Navbar -->
                 <ul class="nav navbar-nav navbar-right">
