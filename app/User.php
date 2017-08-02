e5608a52909a41ef69c982a401346e7750728761
pr3a <wisit.lertsakwimarn@gmail.com>
2017-08-03 02:00:53 +0700

    Rename method profiles to profile

--- app/User.php
+++ app/User.php
@@ -38,8 +38,8 @@ class User extends Authenticatable
         return $this->belongsToMany('App\Device', 'device_users', 'users_id', 'device_id')->withTimestamps();
     }
 
-    public function profiles() {
-        return $this->hasOne(Profiles::class,'usersp_id');
+    public function profile() {
+        return $this->hasOne(Profile::class,'usersp_id');
     }
 
 }
