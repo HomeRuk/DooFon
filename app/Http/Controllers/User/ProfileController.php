acfda30b7357f43adce6c0045e5dbe9fc16e1871
pr3a <wisit.lertsakwimarn@gmail.com>
2017-08-03 02:23:25 +0700

    Init ProfileController (User)

--- app/Http/Controllers/User/ProfileController.php
+++ app/Http/Controllers/User/ProfileController.php
@@ -6,9 +6,10 @@ use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Requests\ProfileRequest;
 use App\User;
-use App\Profiles;
+use App\Profile;
+use Alert;
 
-class ProfilesController extends Controller
+class ProfileController extends Controller
 {
     /**
      * Device Weather
@@ -18,7 +19,7 @@ class ProfilesController extends Controller
      */
     public function index()
     {
-        $user = User::with('profiles')->find(Auth::user()->id);
+        $user = User::with('profile')->find(Auth::user()->id);
         return view('user.profile.index', [
             'user' => $user,
         ]);// user/profile/index.blade.php
@@ -32,19 +33,21 @@ class ProfilesController extends Controller
                 'name' => $request->name,
                 'email' => $request->email,
             ]);
-            $profiles = Profiles::where('usersp_id','=',$id)->first();
+            $profiles = Profile::where('usersp_id','=',$id)->first();
             if (isset($profiles)) {
                 $profiles->tel = $request->tel;
                 $profiles->save();
             } else {
-                $profiles = new Profiles();
+                $profiles = new Profile();
                 $profiles->tel = $request->tel;
                 $profiles->usersp_id = Auth::user()->id;
                 $profiles->save();
             }
         } catch (\Exception $e) {
+            Alert::error('ปรับปรุงข้อมูลส่วนตัว ผิดพลาด! <br/> กรุณาตรวจสอบข้อมูล!', 'ผลการทำงาน')->persistent('ปิด')->autoclose(3500);
             return back();
         }
+        Alert::success('ปรับปรุงข้อมูลส่วนตัว สำเร็จ!', 'ผลการทำงาน')->persistent('ปิด')->autoclose(3500);
         return back();
     }
 }
